2ec37b968dc30440ab2f7a95310040410a591788
ojas623 <ojasasathe@gmail.com>
2021-06-13 16:38:10 -0700

    change2

--- public/todo.js
+++ public/todo.js
@@ -1,11 +1,57 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
+var enterButton = document.getElementById("enter");
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul");
+var item = document.getElementsByTagName("li");
 
-var app = express();
+function inputLength(){
+	return input.value.length;
+} 
 
-app.set('port', process.env.PORT || 5001);
-app.use(favicon());
-app.use(express.static(path.join(__dirname, './public')));
+function listLength(){
+	return item.length;
+}
 
-app.listen(app.get("port"));
+function createListElement() {
+	var li = document.createElement("li"); // creates an element "li"
+	li.appendChild(document.createTextNode(input.value)); //makes text from input field the li text
+	ul.appendChild(li); //adds li to ul
+	input.value = ""; //Reset text input field
+
+
+	//START STRIKETHROUGH
+	// because it's in the function, it only adds it for new items
+	function crossOut() {
+		li.classList.toggle("done");
+	}
+
+	li.addEventListener("click",crossOut);
+	//END STRIKETHROUGH
+
+
+	// START ADD DELETE BUTTON
+	var dBtn = document.createElement("button");
+	dBtn.appendChild(document.createTextNode("X"));
+	li.appendChild(dBtn);
+	dBtn.addEventListener("click", deleteListItem);
+	// END ADD DELETE BUTTON
+
+
+	//ADD CLASS DELETE (DISPLAY: NONE)
+	function deleteListItem(){
+		li.classList.add("delete")
+	}
+	//END ADD CLASS DELETE
+}
+
+
+function addListAfterClick(){
+	if (inputLength() > 0) { //makes sure that an empty input field doesn't create a li
+		createListElement();
+	}
+}
+
+
+enterButton.addEventListener("click",addListAfterClick);
+
+// TODO: Add Event Listener to create a TODO on 'Enter' Key
+
